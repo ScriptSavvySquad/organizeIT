23f57e777d09465ca4647cacaef749dad18d9c3f
Jannik Kastens <jannik.kastens@hmmh.de>
2024-11-08 10:21:35 +0100

    Add basic appointment layout

---
+++ Models/Appointment.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace organizeIT.Models;
+
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Location { get; set; } = string.Empty;
+} 
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,40 @@
-﻿namespace organizeIT.ViewModels;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using organizeIT.Models;
+using System.Collections.ObjectModel;
+
+namespace organizeIT.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    [ObservableProperty]
+    private ObservableCollection<Appointment> appointments = new();
+
+    [ObservableProperty]
+    private Appointment? selectedAppointment;
+
+    [RelayCommand]
+    private void AddAppointment()
+    {
+        var appointment = new Appointment
+        {
+            Id = Appointments.Count + 1,
+            StartTime = DateTime.Now,
+            EndTime = DateTime.Now.AddHours(1)
+        };
+        
+        Appointments.Add(appointment);
+        SelectedAppointment = appointment;
+    }
+
+    [RelayCommand]
+    private void DeleteAppointment()
+    {
+        if (SelectedAppointment != null)
+        {
+            Appointments.Remove(SelectedAppointment);
+            SelectedAppointment = null;
+        }
+    }
 }
